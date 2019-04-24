1df0f900704e2beea92425e4f1b6894d21a70d8d
git-palace <edeveloper4@gmail.com>
2019-04-24 11:19:07 +0000

    update

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: Duda SSO
+ * Description: SSO plugin between Agent Cloud and Duda
+ * Version:     1.0.0
+ */
+
+if ( !defined( 'ABSPATH' ) ) {
+  exit;
+}
+
+require_once( 'config.php' );
