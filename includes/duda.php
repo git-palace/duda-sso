a915a892b2bbf6198574ec4ec59bcd4ee4b42d85
git-palace <edeveloper4@gmail.com>
2019-04-26 11:29:02 +0000

    update

--- includes/duda.php
+++ includes/duda.php
@@ -3,12 +3,17 @@ class Duda {
   private static $instance = null;
 
   // send get request
-  protected function __http_get( $api_url ) {
+  protected function curl_request( $api_endpoint, $method = 'GET', $body = array() ) {
     $ch = curl_init();
 
-    curl_setopt( $ch, CURLOPT_URL, DUDA_APIENDPOINT . $api_url );
+    curl_setopt( $ch, CURLOPT_URL, DUDA_APIENDPOINT . $api_endpoint );
+    
     curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1) ;
-    curl_setopt( $ch, CURLOPT_CUSTOMREQUEST, 'GET' );
+    
+    curl_setopt( $ch, CURLOPT_CUSTOMREQUEST, $method );
+
+    if ( $method == 'POST' )
+      curl_setopt( $ch, CURLOPT_POSTFIELDS, json_encode( $body ) );
 
     curl_setopt( $ch, CURLOPT_USERPWD, DUDA_APIUSERNAME . ':' . DUDA_APIPASSWORD );
 
@@ -16,7 +21,7 @@ class Duda {
     $headers[] = 'Content-Type: application/json';
     curl_setopt( $ch, CURLOPT_HTTPHEADER, $headers );
 
-    $result = curl_exec( $ch );
+    $response = curl_exec( $ch );
     if ( curl_errno( $ch ) ) {
         error_log( curl_error( $ch ) );
 
@@ -24,7 +29,7 @@ class Duda {
     }
     curl_close ($ch);
 
-    return json_decode( $result, true );
+    return json_decode( $response, true );
   }
 
   // get API
@@ -43,25 +48,9 @@ class Duda {
 
   // get template
   function getTemplates() {
-    // $result = $this->__http_get( '/sites/multiscreen/templates' );
-    $result = wp_remote_get( 
-      DUDA_APIENDPOINT . '/sites/multiscreen/templates', [ 
-      'headers' => [ 
-        'Authorization' => 'Basic ' . base64_encode( DUDA_APIUSERNAME . ':' . DUDA_APIPASSWORD),
-        'Content-Type'  => 'application/json'
-        ] 
-      ] 
-    );
+    $response = $this->curl_request( '/sites/multiscreen/templates' );
 
-    if ( is_wp_error( $result ) ) {
-      echo '<pre>';
-      print_r( $result );
-      echo '</pre>';
-      exit;
-    }
-
-
-    return $result;
+    return $response;
   }
 
   // select template
@@ -69,6 +58,48 @@ class Duda {
     if ( empty( $tpl_id ) )
       return;
     
+    $response = $this->curl_request( '/sites/multiscreen/create', 'POST', [ 'template_id' => $tpl_id ] );
+    
+    if ( is_wp_error( $response ) || !array_key_exists( 'site_name', $response ) ){
+      error_log( "Error occured when create site with template id: " . $tpl_id );
+      return false;
+    }
+    
+    return $response['site_name'];
+  }
 
+  // create customer account
+  function createCustomerAcct( $site_name = null) {
+    $current_user = wp_get_current_user();
+
+    $response = $this->curl_request( '/accounts/create', 'POST', ['account_name'  => $current_user->user_email] );
+    
+    if ( is_wp_error( $response ) ) {
+      return false;
+    }
+
+    $response = $this->curl_request( sprintf( '/accounts/%s/sites/%s/permissions', $current_user->user_email, $site_name ), 'POST', [
+      'permissions' => ['PUSH_NOTIFICATIONS','REPUBLISH','EDIT','INSITE','PUBLISH','CUSTOM_DOMAIN','RESET','SEO','STATS_TAB','BLOG']
+    ] );
+    
+    if ( is_wp_error( $response ) ) {
+      return false;
+    }
+
+    $duda_sso_token = get_user_meta( get_current_user_id(), 'duda_sso_token', true );
+
+    if ( empty( $duda_sso_token ) ) {
+      $response = $this->curl_request( sprintf( '/accounts/sso/%s/token', $current_user->user_email ) );
+  
+      if ( is_wp_error( $response ) || !array_key_exists( 'url_parameter', $response ) ) {
+        error_log( "Error occured when generate SSO Token" );
+        return false;
+      }
+  
+      $duda_sso_token = $response['url_parameter']['name'] . '=' . $response['url_parameter']['value'];
+      update_user_meta( get_current_user_id(), 'duda_sso_token', $duda_sso_token );
+    }
+
+    return DUDA_SSO_ENDPOINT . '/editor/d1?reset=' . $site_name . '&' . $duda_sso_token;
   }
 }
--- includes/index.php
+++ includes/index.php
@@ -4,13 +4,28 @@ require_once( 'duda.php' );
 function duda() {
   return Duda::getInstance();
 }
+add_action( 'init', function() {
+  if ( isset( $_GET['action'] ) ) {
+    switch( $_GET['action'] ) {
+      case 'duda_tpl_select':
+        if ( isset( $_GET['id'] ) && !empty( $_GET['id']) ) {
+          $site_name = duda()->selectTemplate( $_GET['id'] );
 
-if ( isset( $_GET['action'] ) ) {
-  switch( $_GET['action'] ) {
-    case 'duda_tpl_select':
-      if ( isset( $_GET['id'] ) && !empty( $_GET['id']) )
-        duda()->selectTemplate( $_GET['id'] );
-      break;
-    
+          if ( empty( $site_name ) ) {
+            error_log( "Error Occured when create site. Template ID ===> " . $_GET['id'] );
+            return;
+          }
+
+          $url = duda()->createCustomerAcct( $site_name );
+          
+          if ( !empty( $url ) ) {
+            wp_redirect( $url );
+            exit;
+          }
+          
+        }
+        break;
+      
+    }
   }
-}
+} );
