dc6cd03fe10a9c5fc170270eefda52351a38a124
git-palace <edeveloper4@gmail.com>
2019-06-14 03:52:23 -0700

    update duda content

--- includes/duda.php
+++ includes/duda.php
@@ -1,222 +1,304 @@
 <?php
 class Duda {  
-  private static $instance = null;
+	private static $instance = null;
 
-  // send get request
-  protected function curl_request( $api_endpoint, $method = 'GET', $body = array() ) {
-    $ch = curl_init();
+	// send get request
+	protected function curl_request( $api_endpoint, $method = 'GET', $body = array() ) {
+		$ch = curl_init();
 
-    curl_setopt( $ch, CURLOPT_URL, DUDA_APIENDPOINT . $api_endpoint );
-    
-    curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1) ;
-    
-    curl_setopt( $ch, CURLOPT_CUSTOMREQUEST, $method );
+		curl_setopt( $ch, CURLOPT_URL, DUDA_APIENDPOINT . $api_endpoint );
+		
+		curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1) ;
+		
+		curl_setopt( $ch, CURLOPT_CUSTOMREQUEST, $method );
 
-    if ( $method == 'POST' )
-      curl_setopt( $ch, CURLOPT_POSTFIELDS, json_encode( $body ) );
+		if ( $method == 'POST' )
+			curl_setopt( $ch, CURLOPT_POSTFIELDS, json_encode( $body ) );
 
-    curl_setopt( $ch, CURLOPT_USERPWD, DUDA_APIUSERNAME . ':' . DUDA_APIPASSWORD );
+		curl_setopt( $ch, CURLOPT_USERPWD, DUDA_APIUSERNAME . ':' . DUDA_APIPASSWORD );
 
-    $headers = array();
-    $headers[] = 'Content-Type: application/json';
-    curl_setopt( $ch, CURLOPT_HTTPHEADER, $headers );
+		$headers = array();
+		$headers[] = 'Content-Type: application/json';
+		curl_setopt( $ch, CURLOPT_HTTPHEADER, $headers );
 
-    $response = curl_exec( $ch );
-    if ( curl_errno( $ch ) ) {
-        // error_log( curl_error( $ch ) );
+		$response = curl_exec( $ch );
+		if ( curl_errno( $ch ) ) {
+				// error_log( curl_error( $ch ) );
 
-        return false;
-    }
-    curl_close ($ch);
+				return false;
+		}
+		curl_close ($ch);
 
-    return json_decode( $response, true );
-  }
+		return json_decode( $response, true );
+	}
 
-  static function getInstance() {
-    if ( is_null( self::$instance ) ) {
-      self::$instance = new Duda();
-    }
+	static function getInstance() {
+		if ( is_null( self::$instance ) ) {
+			self::$instance = new Duda();
+		}
 
-    return self::$instance;
-  }
+		return self::$instance;
+	}
 
-  // constructor
-  function __construct() {
-    add_action( 'woocommerce_subscription_status_active', [$this, 'duda_subscription_complete'], 10, 1 );
-    add_action( 'woocommerce_subscription_payment_complete', [$this, 'duda_subscription_complete'], 10, 1 );
-    add_action( 'woocommerce_subscription_renewal_payment_complete', [$this, 'duda_subscription_complete'], 10, 1 );
+	// constructor
+	function __construct() {
+		add_action( 'woocommerce_subscription_status_active', [$this, 'duda_subscription_complete'], 10, 1 );
+		add_action( 'woocommerce_subscription_payment_complete', [$this, 'duda_subscription_complete'], 10, 1 );
+		add_action( 'woocommerce_subscription_renewal_payment_complete', [$this, 'duda_subscription_complete'], 10, 1 );
 
-    add_action( 'woocommerce_subscription_status_pending-cancel', [$this, 'duda_subscription_failed'], 10, 1 );
-    add_action( 'woocommerce_subscription_status_cancelled', [$this, 'duda_subscription_failed'], 10, 1 );
-    add_action( 'woocommerce_subscription_status_expired', [$this, 'duda_subscription_failed'], 10, 1 );
-    add_action( 'woocommerce_subscription_status_on-hold', [$this, 'duda_subscription_failed'], 10, 1 );
-    add_action( 'woocommerce_subscription_payment_failed', [$this, 'duda_subscription_failed'], 10, 1 );
-    add_action( 'woocommerce_subscription_renewal_payment_failed', [$this, 'duda_subscription_failed'], 10, 1 );
-  }
+		add_action( 'woocommerce_subscription_status_pending-cancel', [$this, 'duda_subscription_failed'], 10, 1 );
+		add_action( 'woocommerce_subscription_status_cancelled', [$this, 'duda_subscription_failed'], 10, 1 );
+		add_action( 'woocommerce_subscription_status_expired', [$this, 'duda_subscription_failed'], 10, 1 );
+		add_action( 'woocommerce_subscription_status_on-hold', [$this, 'duda_subscription_failed'], 10, 1 );
+		add_action( 'woocommerce_subscription_payment_failed', [$this, 'duda_subscription_failed'], 10, 1 );
+		add_action( 'woocommerce_subscription_renewal_payment_failed', [$this, 'duda_subscription_failed'], 10, 1 );
+	}
 
-  function duda_subscription_complete( $subscription ) {
-    $site_name = $subscription->get_meta( 'site_name' );
-    $initial_subscription = $subscription->get_meta('_subscription_resubscribe');
+	function duda_subscription_complete( $subscription ) {
+		$site_name = $subscription->get_meta( 'site_name' );
+		$initial_subscription = $subscription->get_meta('_subscription_resubscribe');
 
-    if ( empty( $site_name ) && !empty( $initial_subscription)) {
-      $initial_subscription = wc_get_order( $initial_subscription );
+		if ( empty( $site_name ) && !empty( $initial_subscription)) {
+			$initial_subscription = wc_get_order( $initial_subscription );
 
-      $site_name = $initial_subscription->get_meta( 'site_name' );
-    }
+			$site_name = $initial_subscription->get_meta( 'site_name' );
+		}
 
-    $current_user = wp_get_current_user();
-    // $user_email = $subscription->get_billing_email();
-    $user_email = $current_user->user_email;
+		$current_user = wp_get_current_user();
+		// $user_email = $subscription->get_billing_email();
+		$user_email = $current_user->user_email;
 
-    $this->createCustomerAcct( $site_name, $user_email, true );
-    // $this->set_site_publish_mode( $site_name, true );
-  }
+		$this->createCustomerAcct( $site_name, $user_email, true );
+		$this->update_site_content( $site_name );
+		// $this->set_site_publish_mode( $site_name, true );
+	}
 
-  function duda_subscription_failed( $subscription ) {
-    if ( is_int( $subscription ) )
-      $subscription = wc_get_order( $subscription );
-    
-    $site_name = $subscription->get_meta( 'site_name' );
-    $initial_subscription = $subscription->get_meta('_subscription_resubscribe');
+	function duda_subscription_failed( $subscription ) {
+		if ( is_int( $subscription ) )
+			$subscription = wc_get_order( $subscription );
+		
+		$site_name = $subscription->get_meta( 'site_name' );
+		$initial_subscription = $subscription->get_meta('_subscription_resubscribe');
 
-    if ( empty( $site_name ) && !empty( $initial_subscription)) {
-      $initial_subscription = wc_get_order( $initial_subscription );
+		if ( empty( $site_name ) && !empty( $initial_subscription)) {
+			$initial_subscription = wc_get_order( $initial_subscription );
 
-      $site_name = $initial_subscription->get_meta( 'site_name' );
-    }
+			$site_name = $initial_subscription->get_meta( 'site_name' );
+		}
 
-    $current_user = wp_get_current_user();
-    // $user_email = $subscription->get_billing_email();
-    $user_email = $current_user->user_email;
+		$current_user = wp_get_current_user();
+		// $user_email = $subscription->get_billing_email();
+		$user_email = $current_user->user_email;
 
-    $this->deleteCustomerAcct( $site_name, $user_email );
-    // $this->set_site_publish_mode( $site_name, false );
-  }
+		$this->deleteCustomerAcct( $site_name, $user_email );
+		// $this->set_site_publish_mode( $site_name, false );
+	}
 
-  // get template
-  function getTemplates() {
-    $response = $this->curl_request( '/sites/multiscreen/templates' );
+	// get template
+	function getTemplates() {
+		$response = $this->curl_request( '/sites/multiscreen/templates' );
 
-    return $response;
-  }
+		return $response;
+	}
 
-  // select template
-  function selectTemplate( $tpl_id = null, $addon_ids = [] ) {
-    if ( empty( $tpl_id ) )
-      return;
-    
-    $response = $this->curl_request( '/sites/multiscreen/create', 'POST', [ 'template_id' => $tpl_id ] );
-    
-    if ( is_wp_error( $response ) || !array_key_exists( 'site_name', $response ) ){
-      // error_log( "Error occured when create site with template id: " . $tpl_id );
-      return false;
-    }
-    
-    WC()->cart->empty_cart();
-    WC()->cart->add_to_cart( DUDA_SUBSCRIPTION_PRODUCT_ID );
-    
-    foreach( $addon_ids as $addon_id )
-      WC()->cart->add_to_cart( $addon_id );
-    
-    wp_redirect( esc_url( add_query_arg( 'site_name', $response['site_name'], wc_get_checkout_url() ) ) );
-    exit;
-  }
+	// select template
+	function selectTemplate( $tpl_id = null, $addon_ids = [] ) {
+		if ( empty( $tpl_id ) )
+			return;
+		
+		$response = $this->curl_request( '/sites/multiscreen/create', 'POST', [ 'template_id' => $tpl_id ] );
+		
+		if ( is_wp_error( $response ) || !array_key_exists( 'site_name', $response ) ){
+			// error_log( "Error occured when create site with template id: " . $tpl_id );
+			return false;
+		}
+		
+		WC()->cart->empty_cart();
+		WC()->cart->add_to_cart( DUDA_SUBSCRIPTION_PRODUCT_ID );
+		
+		foreach( $addon_ids as $addon_id )
+			WC()->cart->add_to_cart( $addon_id );
+		
+		wp_redirect( esc_url( add_query_arg( 'site_name', $response['site_name'], wc_get_checkout_url() ) ) );
+		exit;
+	}
 
-  // create customer account
-  function createCustomerAcct( $site_name = null, $user_email = null, $is_completed = true ) {
-    if ( empty( $user_email ) )
-      return false;
-    
-    $response = $this->curl_request( '/accounts/create', 'POST', ['account_name'  => $user_email] );
-    
-    if ( is_wp_error( $response ) ) {
-      return false;
-    }
+	// create customer account
+	function createCustomerAcct( $site_name = null, $user_email = null, $is_completed = true ) {
+		if ( empty( $user_email ) )
+			return false;
+		
+		$response = $this->curl_request( '/accounts/create', 'POST', ['account_name'  => $user_email] );
+		
+		if ( is_wp_error( $response ) ) {
+			return false;
+		}
 
-    if ( empty( $site_name ) )
-      return false;
+		if ( empty( $site_name ) )
+			return false;
 
-    $response = $this->curl_request( sprintf( '/accounts/%s/sites/%s/permissions', $user_email, $site_name ), 'POST', [
-      'permissions' => $is_completed ? ['PUSH_NOTIFICATIONS','REPUBLISH','EDIT','INSITE','PUBLISH','CUSTOM_DOMAIN','RESET','SEO','STATS_TAB','BLOG'] : ['PUSH_NOTIFICATIONS', 'EDIT', 'STATS_TAB']
-    ] );
-    
-    if ( is_wp_error( $response ) ) {
-      return false;
-    }
+		$response = $this->curl_request( sprintf( '/accounts/%s/sites/%s/permissions', $user_email, $site_name ), 'POST', [
+			'permissions' => $is_completed ? ['PUSH_NOTIFICATIONS','REPUBLISH','EDIT','INSITE','PUBLISH','CUSTOM_DOMAIN','RESET','SEO','STATS_TAB','BLOG'] : ['PUSH_NOTIFICATIONS', 'EDIT', 'STATS_TAB']
+		] );
+		
+		if ( is_wp_error( $response ) ) {
+			return false;
+		}
 
-    return true;
-  }
+		return true;
+	}
 
-  // delete customer account
-  function deleteCustomerAcct( $site_name = null, $user_email = null ) {
-    if ( empty( $site_name ) || empty( $user_email ) )
-      return;
-    
-    $response = $this->curl_request( sprintf( '/accounts/%s/sites/%s/permissions', $user_email, $site_name ), 'DELETE' );
-    
-    if ( is_wp_error( $response ) ) {
-      return false;
-    }
+	// delete customer account
+	function deleteCustomerAcct( $site_name = null, $user_email = null ) {
+		if ( empty( $site_name ) || empty( $user_email ) )
+			return;
+		
+		$response = $this->curl_request( sprintf( '/accounts/%s/sites/%s/permissions', $user_email, $site_name ), 'DELETE' );
+		
+		if ( is_wp_error( $response ) ) {
+			return false;
+		}
 
-    return true;
-  }
+		return true;
+	}
 
-  function redirect_to_duda( $site_name = null, $user_email = null, $open_new_tab = true ) {
-    if ( empty( $site_name ) || empty( $user_email ) )
-      return;
-    
-    $response = $this->curl_request( sprintf( '/accounts/sso/%s/link/?target=EDITOR&site_name=%s', $user_email, $site_name ) );
-    if ( is_wp_error( $response ) || !array_key_exists( 'url', $response ) ) {
-      // error_log( "Error occured when generate SSO Token" );
-      return false;
-    }
+	function redirect_to_duda( $site_name = null, $user_email = null, $open_new_tab = true ) {
+		if ( empty( $site_name ) || empty( $user_email ) )
+			return;
+		
+		$response = $this->curl_request( sprintf( '/accounts/sso/%s/link/?target=EDITOR&site_name=%s', $user_email, $site_name ) );
+		if ( is_wp_error( $response ) || !array_key_exists( 'url', $response ) ) {
+			// error_log( "Error occured when generate SSO Token" );
+			return false;
+		}
 
-    if ( !$open_new_tab ) {
-      wp_redirect( $response['url'] );
-      exit;
-    }
+		if ( !$open_new_tab ) {
+			wp_redirect( $response['url'] );
+			exit;
+		}
 
-    ob_start();
-    ?>
-      <script>
-        var w = window.open( '', 'WebsiteBuilder' );
-        setTimeout( function() {
-          w.location.href = '<?php _e( $response['url'] ); ?>';
-        }, 100);
-      </script>
-    <?php
+		ob_start();
+		?>
+			<script>
+				var w = window.open( '', 'WebsiteBuilder' );
+				setTimeout( function() {
+					w.location.href = '<?php _e( $response['url'] ); ?>';
+				}, 100);
+			</script>
+		<?php
 
-    $script = ob_get_contents();
+		$script = ob_get_contents();
 
-    ob_end_clean();
+		ob_end_clean();
 
-    echo $script;
-  }
+		echo $script;
+	}
 
-  // redirect to duda editor page
-  function redirect_to_duda_editor() {
-    if ( $_REQUEST['confirm'] != 'yes')
-      return;
+	// redirect to duda editor page
+	function redirect_to_duda_editor() {
+		if ( $_REQUEST['confirm'] != 'yes')
+			return;
 
-    if ( !wcs_user_has_subscription( get_current_user_id(), DUDA_SUBSCRIPTION_PRODUCT_ID, 'active' ) ) {
-      wp_redirect( home_url( '/websitebuilder' ) );
-      exit;
-    }
-    
-    $current_user = wp_get_current_user();
-    $response = $this->curl_request( sprintf( '/accounts/sso/%s/link', $current_user->user_email ) );
+		if ( !wcs_user_has_subscription( get_current_user_id(), DUDA_SUBSCRIPTION_PRODUCT_ID, 'active' ) ) {
+			wp_redirect( home_url( '/websitebuilder' ) );
+			exit;
+		}
+		
+		$current_user = wp_get_current_user();
+		$response = $this->curl_request( sprintf( '/accounts/sso/%s/link', $current_user->user_email ) );
 
-    if ( is_wp_error( $response ) || !array_key_exists( 'url', $response ) ) {
-      // error_log( "Error occured when generate SSO Token" );
-      return false;
-    }
+		if ( is_wp_error( $response ) || !array_key_exists( 'url', $response ) ) {
+			// error_log( "Error occured when generate SSO Token" );
+			return false;
+		}
 
-    wp_redirect( $response['url'] );
-    exit;
-  }
+		wp_redirect( $response['url'] );
+		exit;
+	}
 
-  // get full name
-  function get_full_name() {
+	// update site content
+	function update_site_content( $site_name = null ) {
+		if ( empty( $site_name ) ) return;
 
-  }
+		$current_user = wp_get_current_user();
+		$email = $current_user->user_email;
+		$full_name = $current_user->user_firstname . ' ' . $current_user->user_lastname;
+
+		$office_address_1 = get_user_meta( get_current_user_id(), 'office_address_1', true );
+		$office_address_2 = get_user_meta( get_current_user_id(), 'office_address_2', true );
+		$office_city = get_user_meta( get_current_user_id(), 'office_city', true );
+		$office_state = get_user_meta( get_current_user_id(), 'office_state', true );
+		$office_zip = get_user_meta( get_current_user_id(), 'office_zip', true );
+
+		$office_full_address = sprintf( 
+			'%s<br/>%s, %s %s',
+			$office_address_1 . ( empty( $office_address_2 ) ? '' : ', ' . $office_address_2 ),
+			$office_city,
+			$office_state,
+			$office_zip
+		);
+
+		$office_dre_number = get_user_meta( get_current_user_id(), 'office_dre_number', true );
+		$office_dre_number = str_replace( 'dre', '', strtolower( $office_dre_number ) );
+		$office_dre_number = str_replace( '#', '', strtolower( $office_dre_number ) );
+
+		$dre_number = get_user_meta( get_current_user_id(), 'dre_number', true );
+		$dre_number = str_replace( 'dre', '', strtolower( $dre_number ) );
+		$dre_number = str_replace( '#', '', strtolower( $dre_number ) );
+
+		$employee = new WeDevs\ERP\HRM\Employee( get_current_user_id() );
+		$work_phone = $employee->get_work_phone();
+		$description = $employee->get_data()['personal']['description'];
+
+		$custom_content = [];
+
+		if ( !empty( $full_name ) ) {
+			$custom_content[] = [
+				'label' => 'Full Name',
+				'text'  => sprintf( '<p class="rteBlock">%s</p>', $full_name)
+			];
+		}
+
+		if ( !empty( $work_phone ) ){
+			$custom_content[] = [
+				'label' => 'Work Phone',
+				'text'  => sprintf( '<p class="rteBlock">%s</p>', $work_phone )
+			];
+		}
+
+		if ( !empty( $email ) ){
+			$custom_content[] = [
+				'label' => 'Email Address',
+				'text'  => sprintf( '<p class="rteBlock">%s</p>', $email )
+			];
+		}
+
+		if ( !empty( $office_dre_number ) ) {
+			$custom_content[] = [
+				'label' => 'DRE Number',
+				'text'  => sprintf( '<p class="rteBlock">DRE# %s</p>', $office_dre_number )
+			];
+		} elseif ( !empty( $dre_number ) ) {
+			$custom_content[] = [
+				'label' => 'DRE Number',
+				'text'  => sprintf( '<p class="rteBlock">DRE# %s</p>', $dre_number )
+			];
+		}
+
+		if ( !empty( $description ) ) {
+			$custom_content[] = [
+				'label'	=> 'Biography',
+				'text'  => sprintf( '<p class="rteBlock">%s</p>', $description )
+			];
+		}
+
+		if ( !empty( $description ) ) {
+			$custom_content[] = [
+				'label'	=> 'Office Address',
+				'text'  => sprintf( '<p class="rteBlock">%s</p>', $office_full_address )
+			];
+		}
+
+		$response = $this->curl_request( sprintf( '/sites/multiscreen/%s/content', $site_name ), 'POST', ['site_texts'  => ['custom' => $custom_content]] );
+	}
 }
--- index.php
+++ index.php
@@ -6,11 +6,11 @@
  */
 
 if ( !defined( 'ABSPATH' ) ) {
-  exit;
+	exit;
 }
 
 if ( !is_plugin_active( 'wp-erp/wp-erp.php' ) || !is_plugin_active( 'wp-erp-shortcodes/index.php' ) ) {
-  return;
+	return;
 }
 
 define( 'DUDA_PLUGIN_PATH', plugin_dir_path( __FILE__ ) );
@@ -20,121 +20,114 @@ require_once( 'config.php' );
 require_once( 'includes/index.php' );
 
 if ( function_exists( 'duda' ) ) {
-  duda();
+	duda();
 }
 
 add_action( 'show_duda_addon_remove_button', function( $cart_item_key, $_product ) {
-  $product_id = $_product->get_id();
+	$product_id = $_product->get_id();
 
-  echo apply_filters( 
-    'woocommerce_cart_item_remove_link', 
-    sprintf(
-      '<a href="%s" class="remove" aria-label="%s" data-product_id="%s" data-product_sku="%s" style="%s">&times;</a>', 
-      !in_array( $product_id, array_values( get_duda_subscription_addons() ) ) ? '#' : esc_url( WC()->cart->get_remove_url( $cart_item_key ) ), 
-      __( 'Remove this item', 'woocommerce' ), 
-      esc_attr( $product_id ), 
-      esc_attr( $_product->get_sku() ),
-      !in_array( $product_id, array_values( get_duda_subscription_addons() ) ) ? 'visibility: hidden' : ''
-    ), 
-    $cart_item_key 
-  );
+	echo apply_filters( 
+		'woocommerce_cart_item_remove_link', 
+		sprintf(
+			'<a href="%s" class="remove" aria-label="%s" data-product_id="%s" data-product_sku="%s" style="%s">&times;</a>', 
+			!in_array( $product_id, array_values( get_duda_subscription_addons() ) ) ? '#' : esc_url( WC()->cart->get_remove_url( $cart_item_key ) ), 
+			__( 'Remove this item', 'woocommerce' ), 
+			esc_attr( $product_id ), 
+			esc_attr( $_product->get_sku() ),
+			!in_array( $product_id, array_values( get_duda_subscription_addons() ) ) ? 'visibility: hidden' : ''
+		), 
+		$cart_item_key 
+	);
 }, 10, 2);
 
 add_action( 'wp_enqueue_scripts', function() {
-  wp_register_style( 'select2-css', 'https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.7/css/select2.min.css' );
-  wp_register_script( 'select2-js', 'https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.7/js/select2.min.js', array( 'jquery' ) );
+	wp_register_style( 'select2-css', 'https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.7/css/select2.min.css' );
+	wp_register_script( 'select2-js', 'https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.7/js/select2.min.js', array( 'jquery' ) );
 
-  wp_register_style( 'duda-sso-style', plugin_dir_url( __FILE__ ) . 'assets/css/style.css', array( 'select2-css' ) );
-  wp_register_script( 'duda-sso-script', plugin_dir_url( __FILE__ ) . 'assets/js/scripts.js', array( 'jquery', 'select2-js' ), '1.0.0', true );
+	wp_register_style( 'duda-sso-style', plugin_dir_url( __FILE__ ) . 'assets/css/style.css', array( 'select2-css' ) );
+	wp_register_script( 'duda-sso-script', plugin_dir_url( __FILE__ ) . 'assets/js/scripts.js', array( 'jquery', 'select2-js' ), '1.0.0', true );
 
-  wp_enqueue_style( 'duda-sso-style' );
-  
-  if ( is_account_page() || is_checkout() ) {
-    wp_enqueue_style( 'duda-woocommerce-style', plugin_dir_url( __FILE__ ) . 'assets/css/woocommerce.css' );
-  }
+	wp_enqueue_style( 'duda-sso-style' );
+	
+	if ( is_account_page() || is_checkout() ) {
+		wp_enqueue_style( 'duda-woocommerce-style', plugin_dir_url( __FILE__ ) . 'assets/css/woocommerce.css' );
+	}
 } );
 
 add_shortcode( '10-neightborhoods-form-view', function() {
-  $file_path = DUDA_PLUGIN_PATH . 'assets/js/neighborhoods.json';
-  $marketplaces = file_exists( $file_path ) ? json_decode( file_get_contents( $file_path ), true ) : [];
+	$file_path = DUDA_PLUGIN_PATH . 'assets/js/neighborhoods.json';
+	$marketplaces = file_exists( $file_path ) ? json_decode( file_get_contents( $file_path ), true ) : [];
 
-  // wp_localize_script( 'duda-sso-script', 'ajaxurl', admin_url( 'admin-ajax.php' ) );
-  // wp_localize_script( 'duda-sso-script', 'marketplaces', $marketplaces );
-  wp_enqueue_script( 'duda-sso-script' );
-  
-  ob_start();
+	// wp_localize_script( 'duda-sso-script', 'ajaxurl', admin_url( 'admin-ajax.php' ) );
+	// wp_localize_script( 'duda-sso-script', 'marketplaces', $marketplaces );
+	wp_enqueue_script( 'duda-sso-script' );
+	
+	ob_start();
 ?>
 
-  <div class="neighborhoods-select-container container">
-    <form method="POST">
-      <div class="marketplace-container">
-        <h2>Select Market Place:</h2>
-        <select name="marketplace" required style="width: 100%">
-          <option value="">Please select below one.</option>
-          
-          <?php foreach ( array_keys( $marketplaces ) as $marketplace ) : ?>
-          
-            <option value="<?php esc_attr_e( $marketplace ); ?>"><?php _e( $marketplace ); ?></option>
-          
-          <?php endforeach; ?>
+	<div class="neighborhoods-select-container container">
+		<form method="POST">
+			<div class="marketplace-container">
+				<h2>Select Market Place:</h2>
+				<select name="marketplace" required style="width: 100%">
+					<option value="">Please select below one.</option>
+					
+					<?php foreach ( array_keys( $marketplaces ) as $marketplace ) : ?>
+					
+						<option value="<?php esc_attr_e( $marketplace ); ?>"><?php _e( $marketplace ); ?></option>
+					
+					<?php endforeach; ?>
 
-        </select>
-      </div>
+				</select>
+			</div>
 
-      
-      <h2>Select Neighborhoods (Max: 10):</h2>
+			
+			<h2>Select Neighborhoods (Max: 10):</h2>
 
-      <?php foreach ( $marketplaces as $marketplace => $neighborhoods ): ?>
-      
-      <div class="neighborhoods-container" marketplace="<?php esc_attr_e( $marketplace ); ?>" style="display: none;">
-        <?php foreach ( $neighborhoods as $neighborhood ): ?>
+			<?php foreach ( $marketplaces as $marketplace => $neighborhoods ): ?>
+			
+			<div class="neighborhoods-container" marketplace="<?php esc_attr_e( $marketplace ); ?>" style="display: none;">
+				<?php foreach ( $neighborhoods as $neighborhood ): ?>
 
-          <label for="<?php esc_attr_e( sanitize_title( $neighborhood ) ); ?>">
-            <input type="checkbox" name="neighborhoods[]" id="<?php esc_attr_e( sanitize_title( $neighborhood ) ); ?>" value="<?php esc_attr_e( $neighborhood ); ?>"/> <?php _e( $neighborhood ); ?>
-          </label>
+					<label for="<?php esc_attr_e( sanitize_title( $neighborhood ) ); ?>">
+						<input type="checkbox" name="neighborhoods[]" id="<?php esc_attr_e( sanitize_title( $neighborhood ) ); ?>" value="<?php esc_attr_e( $neighborhood ); ?>"/> <?php _e( $neighborhood ); ?>
+					</label>
 
-        <?php endforeach; ?>
-      </div>
+				<?php endforeach; ?>
+			</div>
 
-      <?php endforeach; ?>
+			<?php endforeach; ?>
 
-      <input type="hidden" name="action" value="save_neighborhoods" />
-      <input type="hidden" name="order_id" value="<?php esc_attr_e( !empty( $_GET['order_id'] ) ? $_GET['order_id'] : 0 ); ?>"/>
-      <input type="hidden" name="__wpnonce" value="<?php esc_attr_e( wp_create_nonce( '10-neighborhoods' ) ); ?>"/>
+			<input type="hidden" name="action" value="save_neighborhoods" />
+			<input type="hidden" name="order_id" value="<?php esc_attr_e( !empty( $_GET['order_id'] ) ? $_GET['order_id'] : 0 ); ?>"/>
+			<input type="hidden" name="__wpnonce" value="<?php esc_attr_e( wp_create_nonce( '10-neighborhoods' ) ); ?>"/>
 
-      <button type="Submit">Submit</button>
-    </form>
-  </div>
+			<button type="Submit">Submit</button>
+		</form>
+	</div>
 
 <?php
-  $html = ob_get_contents();
+	$html = ob_get_contents();
 
-  ob_end_clean();
+	ob_end_clean();
 
-  return $html;
+	return $html;
 } );
 
 add_shortcode( 'duda-sso-view', function() {
-  /* $current_user = wp_get_current_user();
-  echo '<pre>';
-  print_r( [$current_user->display_name, $current_user->user_email] );
-  echo '</pre>';
+	wp_enqueue_script( 'duda-sso-script' );
 
-  return; */
-
-  wp_enqueue_script( 'duda-sso-script' );
-
-  ob_start();
+	ob_start();
 ?>
 
-  <div class="duda-sso-container container">
-    <?php include_once 'views/all-templates.php'; ?>
-  </div>
+	<div class="duda-sso-container container">
+		<?php include_once 'views/all-templates.php'; ?>
+	</div>
 
 <?php
-  $html = ob_get_contents();
+	$html = ob_get_contents();
 
-  ob_end_clean();
+	ob_end_clean();
 
-  return $html;
+	return $html;
 } );
